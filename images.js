ed994c1c5dad172816ccbb6cce54e64f36307abb
max <max.0x71@gmail.com>
2022-06-11 21:54:43 +0100

    gallery works!!

---
+++ images.js
@@ -0,0 +1,18 @@
+const images = []
+
+for (let i = 1; i < 144; i++) {
+    const image = `/content/imgs/img (${i}).jpg`
+    images.push(image)
+}
+
+console.log(images.length + "length")
+
+while (images.length != 0) {
+    const image = images[Math.floor(Math.random() * images.length)]
+
+    images.splice(images.indexOf(image), 1)
+
+    console.log(images.length)
+
+    $(".images").append(`<img loading="lazy" src="${image}">`)
+}
